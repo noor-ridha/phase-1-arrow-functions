2a97d5c7e50040997a7350daafeca21ed66845ee
noor <noorridha028@gmail.com>
2022-11-03 15:16:18 +0300

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(){
+let a=2000;
+let b=100
+return a/b;
+}
+divide();
+
+const square=() => {
+    let x=2
+    return x*x;
+}
+square();
+
+const add=(a,b) => {
+return a+b;
+}
+add(2,3);
